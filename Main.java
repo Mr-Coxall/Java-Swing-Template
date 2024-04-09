bc4040bd9215d18891a191aa4b1aeacfb81abdc5
Patrick Coxall <mr.coxall@mths.ca>
2024-04-09 16:40:22 +0000

    init

---
+++ Main.java
@@ -0,0 +1,27 @@
+/*
+ * Main.java shows the "Hello, World!" program in Java Swing. 
+ */
+import javax.swing.*;        
+
+public class Main {
+    /**
+     * Create the GUI and shows it.
+     */
+
+    public static void main(String[] args) {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Hello, World! - Swing");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add button
+        JButton aButton = new JButton("Hello, World!");
+        aButton.setBounds(125, 135, 150, 30);
+        frame.add(aButton);
+
+        //Display the window.
+        //frame.pack();
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Java-Swing-Template
+# Java Swing Template
+
+## To View GUI
+
+To view the Java Swing GUI output, do the following:
+- under "Ports" add in port 6080
+- click on the "🌎" that will show up in the port line
+- click on "Connect" on noVNC
+- enter password "vscode"
